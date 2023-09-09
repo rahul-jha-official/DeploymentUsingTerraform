6ca38e7b0b36ad03fb917b4e1e7829141baab468
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2023-09-09 18:08:46 +0530

    Add files via upload

---
+++ 02_AppService/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "MyAppSericeResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "MyAppSericePlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "as" {
+  name                = "MyAppSericeUsingTerraform"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    dotnet_framework_version = "v6.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
